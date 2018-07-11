a0f6181d409fd62d0349f4f8a716dce43af178f0
Aya Sakr <ayasakr91@gmail.com>
2018-07-11 13:19:44 +0300

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  const currentClass = "current";
+  const galleryClass = "gallery-info";
+
+  thumbnails.forEach( function(thumbnail) {
+    thumbnail.addEventListener("click", function(){
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      //Set current in aside thumbnails
+      document.querySelector("."+currentClass).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
